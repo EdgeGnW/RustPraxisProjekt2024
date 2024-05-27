c91f0a491b1f0b1debd64fce3bd8d079a0b4cebe
amer.redzovic <amer.redzovic@stud.uni-due.de>
2024-05-27 16:48:37 +0200

    Idea proposal

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,14 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod wavegraph;
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
+mod test {
+    use crate::wavegraph::Wavegraph;
+    use petgraph::{csr::IndexType, Undirected};
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn check_something() {
+        let mut graph = Wavegraph::new_undirected();
+        let a = graph.add_node("Duisburg", 1.0);
+        let b = graph.add_node("Gelsenkirchen", 0.5);
+        let e = graph.add_edge(a, b, "A40", "34km");
     }
 }
-
-
