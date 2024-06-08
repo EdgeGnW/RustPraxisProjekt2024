791caf7e32e35ba5c233d37d10cc05e2c8fbf640
derfred3 <Der-Fred3@web.de>
2024-06-08 14:38:24 +0200

    Fix test messaging and handling of types for adjacency list

--- src/lib.rs
+++ src/lib.rs
@@ -5,57 +5,69 @@ mod wavegraph;
 #[cfg(test)]
 mod test {
     use crate::graphmodel::GraphModel;
+    use petgraph::graph::DefaultIx;
 
     #[test]
     fn check_adjacency_list_directed() {
         #![allow(unused_variables)]
-        let mut graph = GraphModel::new_directed();
-        let v1 = graph.add_node(String::from("v1"), 1.0);
-        let v2 = graph.add_node(String::from("v2"), 1.5);
-        let v3 = graph.add_node(String::from("v3"), 1.0);
-        let v4 = graph.add_node(String::from("v4"), 2.0);
+        // Specific type annotation is needed here for some reason
+        let mut graph = GraphModel::<&str,f32,f32,petgraph::Directed, DefaultIx>::new_directed();
+        let v1 = graph.add_node("v1", 1.0);
+        let v2 = graph.add_node("v2", 1.5);
+        let v3 = graph.add_node("v3", 1.0);
+        let v4 = graph.add_node("v4", 2.0);
 
         // Edges:
         // v1 -> v2
-        let e1 = graph.add_edge(v1.index().into(), v2.index().into(), String::from("e1"), 1.0);
+        let e1 = graph.add_edge(v1, v2, "e1", 1.0);
         // v1 -> v3
-        let e2 = graph.add_edge(v1.index().into(), v3.index().into(), String::from("e2"), 1.0);
+        let e2 = graph.add_edge(v1, v3, "e2", 1.0);
         // v3 -> v1
-        let e3 = graph.add_edge(v3.index().into(), v1.index().into(), String::from("e3"), 1.0);
+        let e3 = graph.add_edge(v3, v1, "e3", 1.0);
         // v3 -> v2
-        let e4 = graph.add_edge(v3.index().into(), v2.index().into(), String::from("e4"), 1.0);
+        let e4 = graph.add_edge(v3, v2, "e4", 1.0);
         // v3 -> v4
-        let e5 = graph.add_edge(v3.index().into(), v4.index().into(), String::from("e5"), 1.0);
+        let e5 = graph.add_edge(v3, v4, "e5", 1.0);
         // v4 -> v1
-        let e6 = graph.add_edge(v4.index().into(), v1.index().into(), String::from("e6"), 1.0);
+        let e6 = graph.add_edge(v4, v1, "e6", 1.0);
         // v4 -> v2
-        let e7 = graph.add_edge(v4.index().into(), v2.index().into(), String::from("e7"), 1.0);
+        let e7 = graph.add_edge(v4, v2, "e7", 1.0);
 
         let adjacency_list = graph.get_adjacency_list();
 
-        assert!(adjacency_list.len() == 4, "Adjacency list has the wrong length!");
-        assert!(adjacency_list == vec![vec![&String::from("v2"), &String::from("v3")], vec![], vec![&String::from("v1"), &String::from("v2"), &String::from("v4")], vec![&String::from("v1"), &String::from("v2")]]);
+        let expected_len = 4;
+        assert!(adjacency_list.len() == expected_len, 
+            "Adjacency list has the wrong length!\nExpected:\n{0:?}\nFound:\n{1}", expected_len, adjacency_list.len());
+
+        let expected_list = vec![vec![&"v2", &"v3"], vec![], vec![&"v1", &"v2", &"v4"], vec![&"v1", &"v2"]];
+        assert!(adjacency_list == expected_list,
+            "Adjacency list diverges from expectation.\nExpected\n{0:?}\nFound:\n{1:?}", expected_list, adjacency_list);
     }
 
     #[test]
     fn check_adjacency_list_undirected() {
         #![allow(unused_variables)]
         let mut graph = GraphModel::new_undirected();
-        let v1 = graph.add_node(String::from("v1"), 1.0);
-        let v2 = graph.add_node(String::from("v2"), 1.5);
-        let v3 = graph.add_node(String::from("v3"), 1.0);
+        let v1 = graph.add_node("v1", 1.0);
+        let v2 = graph.add_node("v2", 1.5);
+        let v3 = graph.add_node("v3", 1.0);
 
         // Edges:
         // v1 - v2
-        let e1 = graph.add_edge(v1, v2, String::from("e1"), 1.0);
+        let e1 = graph.add_edge(v1, v2, "e1", 1.0);
         // v1 - v3
-        let e2 = graph.add_edge(v1, v3, String::from("e2"), 1.0);
+        let e2 = graph.add_edge(v1, v3, "e2", 1.0);
         // v3 - v2
-        let e4 = graph.add_edge(v3, v2, String::from("e4"), 1.0);
+        let e4 = graph.add_edge(v3, v2, "e4", 1.0);
 
         let adjacency_list = graph.get_adjacency_list();
 
-        assert!(adjacency_list.len() == 3, "Adjacency list has the wrong length!");
-        assert!(adjacency_list == vec![vec![&String::from("v2"), &String::from("v3")], vec![&String::from("v1"), &String::from("v3")], vec![&String::from("v1"), &String::from("v2")]]);
+        let expected_len = 3;
+        assert!(adjacency_list.len() == expected_len, 
+            "Adjacency list has the wrong length!\nExpected:\n{0:?}\nFound:\n{1}", expected_len, adjacency_list.len());
+
+        let expected_list = vec![vec![&"v2", &"v3"], vec![&"v1", &"v3"], vec![&"v1", &"v2"]];
+        assert!(adjacency_list == expected_list, 
+            "Adjacency list diverges from expectation.\nExpected:\n{0:?}\nFound:\n{1:?}", expected_list, adjacency_list);
     }
 }
